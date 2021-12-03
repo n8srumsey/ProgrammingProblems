bcca28bf79919c845190f31cbcb6ed317252d9d2
Nathan Rumsey <nathan.s.rumsey@gmail.com>
2021-12-02 16:21:40 -0800

    AoC 2015 Day6

--- AdventOfCode/2015/Day6/pt1.py
+++ AdventOfCode/2015/Day6/pt1.py
@@ -1,1 +1,54 @@
 # Day6 Part 1 
+
+input_path = "./input.txt"
+
+with open(input_path) as f:
+    commands = f.readlines()
+
+grid = [[False for _ in range(1000)] for _ in range(1000)]
+
+def parse_commands(c: str):
+    if 'toggle' in c:
+        c = c[7:]
+        parsed = c.split()
+        xy1 = [int(i) for i in parsed[0].split(',')]
+        xy2 = [int(i) for i in parsed[2].split(',')]
+        toggle(xy1[0], xy1[1], xy2[0], xy2[1])
+    elif 'turn on' in c:
+        c = c[8:]
+        parsed = c.split()
+        xy1 = [int(i) for i in parsed[0].split(',')]
+        xy2 = [int(i) for i in parsed[2].split(',')]
+        turn_on(xy1[0], xy1[1], xy2[0], xy2[1])
+    elif 'turn off' in c:
+        c = c[9:]
+        parsed = c.split()
+        xy1 = [int(i) for i in parsed[0].split(',')]
+        xy2 = [int(i) for i in parsed[2].split(',')]
+        turn_off(xy1[0], xy1[1], xy2[0], xy2[1])
+
+def turn_on(x1, y1, x2, y2):
+    for i in range(x1, x2 + 1):
+        for j in range(y1, y2 + 1):
+            grid[i][j] = True
+
+def turn_off(x1, y1, x2, y2):
+    for i in range(x1, x2 + 1):
+        for j in range(y1, y2 + 1):
+            grid[i][j] = False
+
+def toggle(x1, y1, x2, y2):
+    for i in range(x1, x2 + 1):
+        for j in range(y1, y2 + 1):
+            grid[i][j] = not grid[i][j]
+
+for command in commands:
+    parse_commands(command)
+
+count = 0
+for i in range(1000):
+    for j in range(1000):
+        if grid[i][j]:
+            count += 1
+
+print(count)
