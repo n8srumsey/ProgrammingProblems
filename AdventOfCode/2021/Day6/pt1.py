25578f9acd0c2926f5dd3d8eb555c5a9fa89a658
Nathan Rumsey <nathan.s.rumsey@gmail.com>
2021-12-05 21:52:53 -0800

    Day 6

--- AdventOfCode/2021/Day6/pt1.py
+++ AdventOfCode/2021/Day6/pt1.py
@@ -1,1 +1,34 @@
 # Day6 Part 1 
+
+data_input = "./input.txt"
+
+with open(data_input) as f:
+    data = [l.rstrip('\n') for l in f.readlines()][0]
+
+class Fish:       
+    def __init__(self, timer) -> None:
+        self.countdown = timer
+        
+    def advance_day(self):
+        if self.countdown == 0:
+            self.countdown = 6
+            return Fish(8)
+        
+        self.countdown -= 1
+        return None
+            
+fish = [Fish(int(fish)) for fish in data.split(',')]
+
+if __name__ == '__main__':
+    fish = [Fish(int(fish)) for fish in data.split(',')]
+    
+    for _ in range(80):
+        new_fishies = []
+        for f in fish:
+            new_fish = f.advance_day()
+            if new_fish is not None:
+                new_fishies.append(new_fish)
+        for f in new_fishies:
+            fish.append(f)
+    
+    print(len(fish))
--- AdventOfCode/2021/Day6/pt2.py
+++ AdventOfCode/2021/Day6/pt2.py
@@ -1,1 +1,24 @@
 # Day6 Part 2 
+data_input = "./input.txt"
+
+with open(data_input) as file:
+    data = [int(f) for f in file.readline().strip().split(',')]
+
+if __name__ == '__main__':
+    cycle_count = [0 for _ in range(10)]
+
+    for time in data:
+        cycle_count[time] += 1
+
+    total_days = 256
+    for day in range(total_days):
+        for i, count in enumerate(cycle_count):
+            if i == 0:
+                cycle_count[7] += count
+                cycle_count[9] += count
+                cycle_count[i] = 0
+            else:
+                cycle_count[i - 1] += count
+                cycle_count[i] = 0
+                
+    print(cycle_count)
