6b5c7cdd5e06c9bcab8038b07ea639b65d10a034
Nathan Rumsey <nathan.s.rumsey@gmail.com>
2021-12-06 21:25:30 -0800

    Day7

--- AdventOfCode/2021/Day7/pt1.py
+++ AdventOfCode/2021/Day7/pt1.py
@@ -1,1 +1,21 @@
 # Day7 Part 1 
+import statistics 
+data_input = "./input.txt"
+
+with open(data_input) as file:
+    data = [int(s) for s in file.readline().rstrip('\n').split(',')]
+    
+def get_average():
+    return statistics.median(data)
+
+def get_fuel(average):
+    fuel = []
+    for crab in data:
+        fuel.append(abs(crab - average))
+    return fuel
+
+if __name__ == '__main__':
+    average = int(get_average())
+    print(average)
+    fuel = get_fuel(average)
+    print(sum(fuel))
--- AdventOfCode/2021/Day7/pt2.py
+++ AdventOfCode/2021/Day7/pt2.py
@@ -1,1 +1,25 @@
 # Day7 Part 2 
+
+import statistics 
+data_input = "./input.txt"
+
+with open(data_input) as file:
+    data = [int(s) for s in file.readline().rstrip('\n').split(',')]
+    
+def get_average():
+    return statistics.mean(data)
+
+def fuel(distance):
+    return sum([i for i in range(1, distance + 1)])
+
+def get_fuel(average):
+    f = []
+    for crab in data:
+        f.append(fuel(abs(crab - average)))
+    return f
+
+if __name__ == '__main__':
+    average = int(get_average())
+    print(average)
+    fuel1 = get_fuel(average)
+    print(sum(fuel1))
